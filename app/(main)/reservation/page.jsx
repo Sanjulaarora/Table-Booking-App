b66422d19697764efb69f4ac9e58a723ddd4eb50
Sanjulaarora <sanjulaarora3005@gmail.com>
2025-01-05 23:27:06 +0530

    removed '/' because of which available slots api was not working :(

--- app/(main)/reservation/page.jsx
+++ app/(main)/reservation/page.jsx
@@ -31,7 +31,7 @@ const ReservationForm = () => {
     setLoading(true);
 
     try {
-      const res = await axios.get(`https://backend-table-booking-app.onrender.com//available-slots`, {
+      const res = await axios.get(`https://backend-table-booking-app.onrender.com/available-slots`, {
         params: { date: date.toISOString().split("T")[0] },
       });
       setAvailableSlots(res.data?.availableSlots);
